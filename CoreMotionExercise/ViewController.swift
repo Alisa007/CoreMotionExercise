72bd696be14596d3f9fd81f798f7d9790bbe547a
alisabelousova <alisabelousova007@gmail.com>
2016-05-30 12:31:50 +0300

    Initial commit

--- CoreMotionExercise/ViewController.swift
+++ CoreMotionExercise/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    let motion: CMMotionManager = CMMotionManager()
 
+    @IBOutlet weak var rollLabel: UILabel!
+    @IBOutlet weak var pitchLabel: UILabel!
+    @IBOutlet weak var avatar: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        if motion.gyroAvailable {
+            motion.gyroUpdateInterval = 0.5
+            motion.startDeviceMotionUpdatesToQueue(.mainQueue()) {
+                data, error in
+
+                let roll: CGFloat = CGFloat((data?.attitude.roll)!)
+                let pitch: CGFloat = CGFloat((data?.attitude.pitch)!)
+                let yaw: CGFloat = CGFloat((data?.attitude.yaw)!)
+
+                self.rollLabel.text = String(format: "%.2f", roll)
+                self.pitchLabel.text = String(format: "%.2f", pitch)
+
+                var rotation: CATransform3D = CATransform3DIdentity
+                rotation = CATransform3DRotate(rotation, pitch - CGFloat(M_PI_2), 1, 0, 0);
+                rotation = CATransform3DRotate(rotation, roll, 0, 1, 0);
+                rotation = CATransform3DRotate(rotation, yaw, 0, 0, 1);
+
+                self.avatar.layer.transform = rotation;
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
